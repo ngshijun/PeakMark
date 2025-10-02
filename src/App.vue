6a101889e81449f570ac651a61670b6f45a45b48
ngshijun <shijunng2010@gmail.com>
2025-10-02 22:44:21 +0800

    add speed insights

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import { RouterView } from 'vue-router'
 import { Toaster } from '@/components/ui/sonner'
 import 'vue-sonner/style.css'
 import { useTheme } from '@/composables/useTheme'
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 
 const { theme } = useTheme()
 </script>
 
 <template>
   <div class="relative min-h-screen">
+    <SpeedInsights />
     <RouterView />
     <Toaster richColors position="top-center" :theme="theme" />
   </div>
